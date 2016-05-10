e56541d42944157cf75b28e11c03fd65036e5b13
dlopez <1ncubuS>
2016-05-10 15:18:29 -0600

    Template

--- CreateStack.sh
+++ CreateStack.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #script for the creation of Mesos Cluster
 cwd=$(pwd)
-STACKNAME=${1}
-STACKTEMPLATEURL='s3://netops-repo/cloudformation/mesos/mesosdcos_singleamaster.template'
+STACKNAME=$1
+STACKTEMPLATEURL='https://raw.githubusercontent.com/DouglasLopez/Mesos-Single-Master/master/cloudformation/DCOSSingleMaster.template'
 REGIONS=(us-east-1 us-west-2)
 
 
@@ -10,7 +10,7 @@ REGIONS=(us-east-1 us-west-2)
 for region in ${REGIONS};
  do
   echo "creation of stack ${STACKNAME} in ${region}"
-  aws cloudformation create-stack --stack-name ${STACKNAME} --template-body ${STACKTEMPLATEURL} --parameters file://${cwd}/bi-dataflow-${region}.json
+  aws cloudformation create-stack --stack-name ${STACKNAME} --template-body ${STACKTEMPLATEURL} --parameters file://${cwd}/parameters.json
 done
 
 
